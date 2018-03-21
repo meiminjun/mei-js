992df835a0e86434ee8de172609ca89071d75834
MEIMINJUN <251222845@qq.com>
2018-03-21 09:32:26 +0800

    删除

--- tools/mock-templates/template/common/index.js
+++
@@ -1,5 +0,0 @@
-<% _.forEach(config.projects, function (project) { %>import * as {{$$.convertUrl(project.name)}} from './{{project.name}}'<% }) %>
-
-export {<% _.forEach(config.projects, function (project, i) { %>
-  {{$$.convertUrl(project.name)}}<% if(config.projects.length - 1 !== i) { %>,<% } %><% }) %>
-}
