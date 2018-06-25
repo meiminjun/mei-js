fb2316d9e711b96172f11b3c6109fff590314c2d
MEIMINJUN <MEIMINJUN077@pingan.com.cn>
2018-06-25 17:16:39 +0800

    更新

--- src/mei/index.js
+++ src/mei/index.js
@@ -3,10 +3,12 @@ import url from './src/url.js'
 import date from './src/date.js'
 import untils from './src/untils.js'
 import money from './src/money.js'
+import array from './src/arr.js'
 
 module.exports = {
   date,
   money,
   untils,
-  url
+  url,
+  array
 }
--- src/mei/src/arr.js
+++ src/mei/src/arr.js
@@ -1,37 +1,3 @@
-// 构造函数继承 TODO:后面与对象继承融合
-function extend (Child, Parent) {
-  var F = function () {}
-  F.prototype = Parent.prototype
-  Child.prototype = new F()
-  Child.prototype.constructor = Child
-  Child.uber = Parent.prototype
-}
-
-// 参考：https://github.com/mqyqingfeng/Blog/issues/22
-// 防抖: 频繁发生的情况下，只有足够时间，才执行一次，例如：坐公交，只要有人刷卡，车子就不会运行
-// debounce(getUserAction, 500);  // 500ms 内只执行一次
-function debounce (func, wait, immediate) {
-  var timeout, result
-  return function () {
-    var context = this
-    var args = arguments
-    if (timeout) clearTimeout(timeout)
-    if (immediate) {
-      // 如果已经执行过，不再执行
-      var callNow = !timeout
-      timeout = setTimeout(function () {
-        timeout = null
-      }, wait)
-      if (callNow) result = func.apply(context, args)
-    } else {
-      timeout = setTimeout(function () {
-        func.apply(context, args)
-      }, wait)
-    }
-    return result
-  }
-}
-
 // 数组去重
 // 第一版
 function unique(array, isSorted) {
@@ -71,8 +37,6 @@ function uniqueArrayObj (array, key) {
 }
 
 export default {
-  extend,
-  debounce,
   uniqueArrayObj,
   unique
 }
